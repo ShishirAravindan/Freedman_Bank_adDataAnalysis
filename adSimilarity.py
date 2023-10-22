ab110f9d6c75099c3d5e4bad754922fa00c25449
ShishirAravindan <37993712+ShishirAravindan@users.noreply.github.com>
2023-10-22 19:36:06 -0400

    Organizing Files (#26)
    * organizing files
    
    * organizing files
    
    ---------
    
    Co-authored-by: Shishir Aravindan <shishiraravindan@Shishirs-MacBook-Air.local>

--- adSimilarity.py
+++ adSimilarity.py
@@ -5,34 +5,49 @@
 # get_ipython().run_line_magic('pip', 'install transformers')
 
 import numpy as np
-
-from transformers import AutoTokenizer, AutoModel
 import torch
+from transformers import AutoTokenizer, AutoModel
 from sklearn.metrics.pairwise import cosine_similarity
+
 def adSimilarity(ad_sentence:str):
     MODEL_NAME = "sentence-transformers/bert-base-nli-mean-tokens"
+    
+    # pre-processing data
     sentences = ad_sentence.split(".")[:-1]
     sentences = [sentence.strip() for sentence in sentences]
+    
+    # Setting up environment variables 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     model = AutoModel.from_pretrained(MODEL_NAME)
     tokens = {'input_ids': [], 'attention_mask': []}
+    
+    # Generating encodings
     for sentence in sentences:
         new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True, padding='max_length', return_tensors='pt')
         tokens['input_ids'].append(new_tokens['input_ids'][0])
         tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+    
     if len(tokens['input_ids']) == 0:
         return (-1,-1)
+    
+    # Vectorization
     tokens['input_ids'] = torch.stack(tokens['input_ids'])
     tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+
+    # Creating embeddings
     outputs = model(**tokens)
     embeddings = outputs.last_hidden_state
+
     attention = tokens['attention_mask']
     mask = attention.unsqueeze(-1).expand(embeddings.shape).float()
     mask_embeddings = embeddings * mask
+
     summed = torch.sum(mask_embeddings, 1)
     counts = torch.clamp(mask.sum(1), min=1e-9)
     mean_pooled = summed / counts
     mean_pooled = mean_pooled.detach().numpy()
+
+    # Building NP-array of Dictionary embeddings
     dict_a=np.load("./staticEmbeddings/prescriptive_stereotypes_embeddings.npy")
     dict_b=np.load("./staticEmbeddings/false_claims_safety_embeddings.npy")
     cosine_similarity_list_a=[]
