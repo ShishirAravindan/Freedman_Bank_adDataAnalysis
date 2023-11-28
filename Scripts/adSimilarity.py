12224ad7a9511dffcef53e5ba53b9f1e5e0c497d
AkhilChoraria <chorariaakhil03@gmail.com>
2023-11-28 00:23:17 -0500

    Fixed some bugs and reshaped the mean pooled values

--- Scripts/adSimilarity.py
+++ Scripts/adSimilarity.py
@@ -16,18 +16,20 @@ def getDictionaryColumns(dictionaryFileName: str):
 
 def getCosineSimilarity(mean_pooled, dictionaryFileName: str):
     DictionaryEmbeddingsFile = os.path.join(os.getcwd(), f"dictionaryEmbeddings/{dictionaryFileName}.pkl")
-
+    if isinstance(mean_pooled,tuple):
+        mean_pooled=np.array(mean_pooled)
+    mean_pooled=mean_pooled.flatten()[:768].reshape(1,-1)
     # Building NP-array of Dictionary embeddings
     with open(DictionaryEmbeddingsFile, "rb") as f:
         deserializedDictEmbeddings = pickle.load(f)
         output=[]
         for dictionary in deserializedDictEmbeddings:
             cosine_similarity_list=[]
-            dict_mean_pooled=deserializedDictEmbeddings[dictionary]
-            
+            dict_mean_pooled=deserializedDictEmbeddings[dictionary].flatten()
+            dict_mean_pooled=dict_mean_pooled.reshape(1,-1)
             for sentence in mean_pooled:
                 try:
-                    cosine_similarity_list.append(cosine_similarity([sentence],dict_mean_pooled))
+                 cosine_similarity_list.append(cosine_similarity([sentence],dict_mean_pooled))
                 except:
                     return [-1]*len(deserializedDictEmbeddings)
             output.append(np.amax(cosine_similarity_list))
